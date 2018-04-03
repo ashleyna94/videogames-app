1fa6152b1651e46e464296286840ef0b150ccd43
andrewprice-ut <andrewprice.ut@gmail.com>
2018-04-03 16:19:47 -0500

    Update app.py

--- videogames/app.py
+++ videogames/app.py
@@ -41,6 +41,13 @@ session = Session(engine)
 def home():
     return render_template("index.html")
 
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+
+
+
 @app.route("/api/genre")
 def genre():
     stmt = session.query(Videogames).statement
