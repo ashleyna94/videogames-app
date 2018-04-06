6715a9bc2e0ff5f617638677b9266b7d367cb9c7
jjlliinn12 <30323487+jjlliinn12@users.noreply.github.com>
2018-04-06 13:13:50 -0500

    Update app.js

--- videogames/static/js/app.js
+++ videogames/static/js/app.js
@@ -1,34 +1,133 @@
-function buildPlot() {
+var data
+
+
+function getData(name) {
     /* data route */
-    var url = "/api/pals";
+    var url = "/api/plot";
     Plotly.d3.json(url, function (error, response) {
-
         console.log(response);
-
-        var data = response
-
-        var layout = {
-            scope: "usa",
-            title: "Pet Pals",
-            showlegend: false,
-            height: 600,
-            // width: 980,
-            geo: {
-                scope: "usa",
-                projection: {
-                    type: "albers usa"
-                },
-                showland: true,
-                landcolor: "rgb(217, 217, 217)",
-                subunitwidth: 1,
-                countrywidth: 1,
-                subunitcolor: "rgb(255,255,255)",
-                countrycolor: "rgb(255,255,255)"
-            },
-        };
-
-        Plotly.newPlot("plot", data, layout);
+        data = response
     });
 }
 
-buildPlot();
+function buildPlot (name) {
+    var dataToRender
+    var cssClass
+    var layoutToRender
+    switch (name) {
+        case "Genre":
+            cssClass = "genre"
+            layoutToRender = {
+                title: 'Game Count vs. Genre (1976-2017)',
+                titlefont: {
+                    size: 30
+                },
+                xaxis: {
+                    title: 'Genre',
+                    titlefont: {
+                        family: 'Arial',
+                        size: 20,
+                        color: '#000000'
+                    }
+                },
+                yaxis: {
+                    title: 'Games',
+                    titlefont: {
+                        family: 'Arial',
+                        size: 20,
+                        color: '#000000'
+                      }
+                    }               
+            }
+            dataToRender = {
+                x: data.Genre.index,
+                y: data.Genre.data,
+                name,
+                type: 'bar'
+            }
+            break
+        case "Platform":
+            cssClass = "platform"
+            layoutToRender = {
+                title: 'Game Count vs. Platform (1976-2017)',
+                titlefont: {
+                    size: 30
+                },  
+                xaxis: {
+                    title: 'Game Platform',
+                    titlefont: {
+                        family: 'Arial',
+                        size: 20,
+                        color: '#000000'
+                    }
+                },
+                yaxis: {
+                    title: 'Games',
+                    titlefont: {
+                        family: 'Arial',
+                        size: 20,
+                        color: '#000000'
+                      }
+                    }               
+            }
+            dataToRender = {
+                x: data.Platform.index,
+                y: data.Platform.data,
+                name,
+                type: 'bar'
+            }
+            break
+        case "Publisher":
+            cssClass = "publisher"
+            layoutToRender = {
+                title: 'Game Count vs. Publisher (1976-2017)',
+                titlefont: {
+                    size: 30
+                },
+                margin: {
+                    l: 100,
+                    r: 100,
+                    b: 250,
+                    t: 55,
+                    pad: 4
+                  },    
+                xaxis: {
+                    title: 'Game Publisher',
+                    titlefont: {
+                        family: 'Arial',
+                        size: 20,
+                        color: '#000000'
+                    }
+                },
+                yaxis: {
+                    title: 'Games',
+                    titlefont: {
+                        family: 'Arial',
+                        size: 20,
+                        color: '#000000'
+                      }
+                    }               
+            }
+            dataToRender = {
+                x: data.Publisher.index,
+                y: data.Publisher.data,
+                name,
+                type: 'bar'
+            }
+            break
+    }
+    document.querySelector("#publisher-plot").classList.add(cssClass)
+    Plotly.newPlot("publisher-plot", [dataToRender], layoutToRender);
+}
+
+
+
+document.querySelector("main select").addEventListener("input", function (e) {
+    var name = e.target.value
+    buildPlot(name)
+})
+
+
+getData()
+
+
